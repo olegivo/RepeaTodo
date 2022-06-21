cd241106a288a288940ba069f3680948ee043ccc
olegivo <olegivo@gmail.com>
2022-06-21 15:14:03 +0300

    isPreview

---
+++ iosApp/iosApp/Utils/EnvironmentValuesExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  EnvironmentValuesExtensions.swift
+//  iosApp
+//
+//  Created by Олег Иващенко on 21.06.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+public extension EnvironmentValues {
+    var isPreview: Bool {
+        #if DEBUG
+        return ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        #else
+        return false
+        #endif
+    }
+}
