dc649cafa82d9079ec84291e913b3cf6d9835ef3
olegivo <olegivo@gmail.com>
2022-08-30 10:54:51 +0300

    Update copyright

--- shared/src/commonTest/kotlin/ru/olegivo/repeatodo/list/presentation/TasksListViewModelImplTest.kt
+++ shared/src/commonTest/kotlin/ru/olegivo/repeatodo/list/presentation/TasksListViewModelImplTest.kt
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2022 Oleg Ivashchenko <olegivo@gmail.com>
+ *
+ * This file is part of RepeaTodo.
+ *
+ * AFS is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * AFS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * RepeaTodo.
+ */
+
 package ru.olegivo.repeatodo.list.presentation
 
 import app.cash.turbine.test
