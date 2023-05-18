f34d9af6c74f4701fc4402c3e6e7598ab468ef5a
olegivo <olegivo@gmail.com>
2023-05-18 21:57:35 +0300

    Inline editor for task creation

--- androidApp/src/main/java/ru/olegivo/repeatodo/android/appModule.kt
+++ androidApp/src/main/java/ru/olegivo/repeatodo/android/appModule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Oleg Ivashchenko <olegivo@gmail.com>
+ * Copyright (C) 2023 Oleg Ivashchenko <olegivo@gmail.com>
  *
  * This file is part of RepeaTodo.
  *
@@ -15,17 +15,12 @@
  * RepeaTodo.
  */
 
-package ru.olegivo.repeatodo.di
+package ru.olegivo.repeatodo.android
 
-import org.koin.androidx.viewmodel.dsl.viewModelOf
-import org.koin.dsl.bind
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
-import ru.olegivo.repeatodo.add.presentation.AddTaskViewModel
-import ru.olegivo.repeatodo.add.presentation.AddTaskViewModelImpl
-import ru.olegivo.repeatodo.main.presentation.MainViewModel
-import ru.olegivo.repeatodo.main.presentation.MainViewModelImpl
+import ru.olegivo.repeatodo.db.DriverFactory
 
-actual fun platformModule() = module {
-    viewModelOf(::MainViewModelImpl).bind<MainViewModel>()
-    viewModelOf(::AddTaskViewModelImpl).bind<AddTaskViewModel>()
+val appModule = module {
+    single { DriverFactory(androidContext()) }
 }
