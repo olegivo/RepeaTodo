f836c1c88be30f5cb3ec7a32c95465bcccf17cd1
olegivo <olegivo@gmail.com>
2023-05-21 15:27:34 +0300

    Add DropDownSelector

---
+++ androidApp/src/main/java/ru/olegivo/repeatodo/android/DropDownSelector.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2023 Oleg Ivashchenko <olegivo@gmail.com>
+ *
+ * This file is part of RepeaTodo.
+ *
+ * RepeaTodo is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License.
+ *
+ * RepeaTodo PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * RepeaTodo.
+ */
+
+package ru.olegivo.repeatodo.android
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Clear
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun <T: Any> DropDownSelector(
+    modifier: Modifier,
+    items: State<List<T>>,
+    initialValue: T?,
+    textSelector: T.() -> String,
+    noneSelectedItem: T? = null,
+    canClear: Boolean = true,
+    onSelected: (T?) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItemIndex by remember { mutableStateOf(items.value.indexOf(initialValue)) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            Row(modifier, verticalAlignment = Alignment.CenterVertically) {
+                val selectedItem = items.value
+                    .takeIf { selectedItemIndex in it.indices }
+                    ?.get(selectedItemIndex)
+                    ?: noneSelectedItem
+                TextField(
+                    value = selectedItem?.textSelector() ?: "",
+                    onValueChange = { },
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    modifier = Modifier
+                        .menuAnchor()
+                )
+                if (canClear) {
+                    IconButton(onClick = {
+                        onSelected(null)
+                        selectedItemIndex = -1
+                    }) {
+                        Icon(Icons.Sharp.Clear, contentDescription = "Clear selection")
+                    }
+                }
+            }
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {
+                    expanded = false
+                }
+            ) {
+                if (noneSelectedItem != null) {
+                    DropdownMenuItem(
+                        text = {
+                            Row {
+                                val isSelected = -1 == selectedItemIndex
+                                Text(
+                                    text = noneSelectedItem.textSelector(),
+                                    fontWeight = if (isSelected) FontWeight.Bold else null
+                                )
+                                Spacer(Modifier.weight(1f))
+                                if (isSelected) {
+                                    Checkbox(
+                                        checked = true,
+                                        enabled = false,
+                                        onCheckedChange = null
+                                    )
+                                }
+                            }
+                        },
+                        onClick = {
+                            selectedItemIndex = -1
+                            expanded = false
+                        }
+                    )
+                }
+                items.value.forEachIndexed { index, item ->
+                    val title = item.textSelector()
+                    DropdownMenuItem(
+                        text = {
+                            Row {
+                                val isSelected = index == selectedItemIndex
+                                Text(
+                                    text = title,
+                                    fontWeight = if (isSelected) FontWeight.Bold else null
+                                )
+                                Spacer(Modifier.weight(1f))
+                                if (isSelected) {
+                                    Checkbox(
+                                        checked = true,
+                                        enabled = false,
+                                        onCheckedChange = null
+                                    )
+                                }
+                            }
+                        },
+                        onClick = {
+                            selectedItemIndex = index
+                            expanded = false
+                            onSelected(item)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ iosApp/RepeaTodo/EditTask/DropdownSelector.swift
@@ -0,0 +1,160 @@
+import Foundation
+import SwiftUI
+
+struct DropdownRow<Item>: View
+where Item: Hashable {
+    var option: Item
+    var onOptionSelected: ((_ option: Item) -> Void)
+    var textSelector: ((_ option: Item) -> String)
+    
+    var body: some View {
+        Button(action: {
+            onOptionSelected(self.option)
+        }) {
+            HStack {
+                Text(textSelector(self.option))
+                    .font(.system(size: 14))
+                    .foregroundColor(Color.black)
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 5)
+    }
+}
+
+struct Dropdown<Item>: View
+where Item: Hashable {
+    
+    var items: [Item]
+    var textSelector: ((_ option: Item) -> String)
+    var onSelected: ((_ option: Item) -> Void)
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(self.items, id: \.self) { option in
+                    DropdownRow(
+                        option: option,
+                        onOptionSelected: onSelected,
+                        textSelector: textSelector
+                    )
+                }
+            }
+        }
+        .frame(minHeight: CGFloat(items.count) * 30, maxHeight: 250)
+        .padding(.vertical, 5)
+        .background(Color.white)
+        .cornerRadius(5)
+        .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+    }
+}
+
+struct DropdownSelector<Item>: View
+where Item: Hashable {
+    @State private var shouldShowDropdown = false
+    var placeholder: String? = nil
+    var items: [Item]
+    @State var selectedItem: Item? = nil
+    var textSelector: ((_ option: Item) -> String)
+    var noneSelectedItem: Item? = nil
+    var canClear: Bool = true
+    var onSelected: ((_ option: Item) -> Void)
+    private let buttonHeight: CGFloat = 45
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                self.shouldShowDropdown.toggle()
+            }) {
+                HStack {
+                    Text(getSelectedText())
+                        .font(.system(size: 14))
+                        .foregroundColor(selectedItem == nil ? Color.gray: Color.black)
+                    
+                    Spacer()
+                    
+                    Image(systemName: self.shouldShowDropdown ? "arrowtriangle.up.fill" : "arrowtriangle.down.fill")
+                        .resizable()
+                        .frame(width: 9, height: 5)
+                        .font(Font.system(size: 9, weight: .medium))
+                        .foregroundColor(Color.black)
+                }
+            }
+            
+            if canClear {
+                Button(action: {
+                    self.selectedItem = nil
+                }) {
+                    Image(systemName: "xmark")
+                        .font(Font.system(size: 9, weight: .medium))
+                        .foregroundColor(Color.black)
+                }
+                .padding(.leading)
+            }
+        }
+        .padding(.horizontal)
+        .cornerRadius(5)
+        .frame(width: .infinity, height: self.buttonHeight)
+        .overlay(
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(Color.gray, lineWidth: 1)
+        )
+        .overlay(
+            VStack {
+                if self.shouldShowDropdown {
+                    Spacer(minLength: buttonHeight + 10)
+                    Dropdown(
+                        items: self.items,
+                        textSelector: textSelector,
+                        onSelected: { option in
+                            shouldShowDropdown = false
+                            selectedItem = option
+                            self.onSelected(option)
+                        }
+                    )
+                }
+            }, alignment: .topLeading
+        )
+        .background(
+            RoundedRectangle(cornerRadius: 5).fill(Color.white)
+        )
+    }
+    
+    private func getSelectedText() -> String {
+        if let selectedItem = selectedItem ?? noneSelectedItem {
+            return textSelector(selectedItem)
+        } else {
+            return placeholder ?? ""
+        }
+    }
+}
+
+struct DropdownSelector_Previews: PreviewProvider {
+    static let items: [String] = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    
+    static var previews: some View {
+        Group {
+            DropdownSelector(
+                items: items,
+                textSelector: { $0 },
+                noneSelectedItem: "Day of the week",
+                onSelected: { option in
+                    print(option)
+                }
+            )
+            .padding(.horizontal)
+        }
+    }
+}
